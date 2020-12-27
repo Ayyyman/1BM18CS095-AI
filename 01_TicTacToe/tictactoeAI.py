53e8c194ec4a7442c08d865aa91fbcb6744be711
Ayyyman <53811550+Ayyyman@users.noreply.github.com>
2020-12-27 23:18:10 +0530

    Update tictactoeAI.py
    Re-Uploaded

--- 01_TicTacToe/tictactoeAI.py
+++ 01_TicTacToe/tictactoeAI.py
@@ -1,7 +1,7 @@
 import random
 
 def drawBoard(board):
-    # This function prints out the board that it was passed.
+    # This function prints out the board that it was passed
 
     # "board" is a list of 10 strings representing the board (ignore index 0)
     print('   |   |')
